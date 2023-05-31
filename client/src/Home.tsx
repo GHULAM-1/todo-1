73e1f078ec05910baf8a1590e984657cf2f861cd
GHULAM-1 <ilyasghulam35@gmail.com>
2023-05-31 18:37:43 +0500

    first commit

---
+++ client/src/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Todo } from "./Todo";
+const Home = () => {
+  return <>
+    <Todo></Todo>
+  </>
+};
+
+export {Home}
---
+++ client/src/Todo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Todo = () => {
+  return <div className="text-red-700">todo</div>;
+};
+
+export {Todo}
